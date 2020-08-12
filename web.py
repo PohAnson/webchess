d6968359af2e973e41ae980af6cfe14e4ea37538
KONG FOOK WAH FOOK <kong.fook.wah_2021@nyjc.edu.sg>
2020-08-12 01:19:44 +0000

    .

--- web.py
+++ web.py
@@ -1,29 +1,30 @@
 from flask import Flask,render_template, request
 from chess import *
 
-app = Flask(__name__)
-ui = WebInterface()
+def main():
+    app = Flask(__name__)
+    ui = WebInterface()
 
-@app.route('/')
-def root():
-    return render_template('homepage.html')
+    @app.route('/')
+    def root():
+        return render_template('homepage.html')
 
-@app.route('/newgame', methods=['POST'])
-def newgame():
-    wname, bname = request.form['wname'], request.form['bname']
-    # players = f"{wname} vs {bname}"
-    board = Board()
-    
-    return render_template('game.html', ui=ui)
+    @app.route('/newgame', methods=['POST'])
+    def newgame():
+        wname, bname = request.form['wname'], request.form['bname']
+        # players = f"{wname} vs {bname}"
+        board = Board()
+        
+        return render_template('game.html', ui=ui)
 
-@app.route('/error', methods=['POST'])
-def error():
-    ui.errmsg='ERROR VARIABLE'
-    return render_template('game.html', ui=ui)
+    @app.route('/error', methods=['POST'])
+    def error():
+        ui.errmsg='ERROR VARIABLE'
+        return render_template('game.html', ui=ui)
 
-@app.route('/promote', methods=['POST'])
-def promote():
-    ui.inputlabel="Which piece do you want to promote?"
-    ui.btnlabel='promote'
-    return render_template('game.html', ui=ui)
-app.run(debug=False)
+    @app.route('/promote', methods=['POST'])
+    def promote():
+        ui.inputlabel="Which piece do you want to promote?"
+        ui.btnlabel='promote'
+        return render_template('game.html', ui=ui)
+    app.run('0.0.0.0', debug=False)
