c47a92ea50112220ac3146948cd3dbe34949f135
PohAnson <pohanson979@gmail.com>
2020-08-21 14:55:36 +0000

    Added auto_test Only have check for checkmate by eating the king

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 from chess import Board, King, Queen, Bishop, Knight, Rook, Pawn
-import web
-# game = Board(debug=False)
-# if game.debug:
-#     game.start()
-#     while game.winner is None:
-#         print('== DISPLAY ==')
-#         game.display()
-#         print('== PROMPT ==')
-#         start, end = game.prompt()
-#         print('== UPDATE ==')
-#         game.update(start, end)
-#         print('== NEXT TURN ==')
-#         game.next_turn()
-# else:
-#     game.start()
-#     while game.winner is None:
-#         game.display()
-#         start, end = game.prompt()
-#         game.update(start, end)
-#         game.next_turn()
-# print(f'Game over. {game.winner} player wins!')
 
-web.main()
+if input('Do you want to auto test for checkmate? (y/n)? ').strip().lower() == 'y':
+  import auto_test
+  auto_test.main()
+else:
+  import web
+  web.main()
