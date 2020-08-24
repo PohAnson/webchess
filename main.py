f9e15cd1406e515f1048c081b85f583e3322b169
PohAnson <pohanson979@gmail.com>
2020-08-24 11:58:08 +0000

    some edits, not sure if will work

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from chess import Board, King, Queen, Bishop, Knight, Rook, Pawn
-
-if input('Do you want to auto test for checkmate? (y/n)? ').strip().lower() == 'y':
-    import auto_test
-    auto_test.main()
-else:
-    import web
-    web.main()
+import auto_test
+import web
+web.main()
