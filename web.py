7104514a140241648b5c7d4c5c4d7a4aaf2450e8
Deston Wong <destonwong8@gmail.com>
2020-08-24 07:36:57 +0000

    winner.html

--- web.py
+++ web.py
@@ -76,6 +76,7 @@ def main():
 
     @app.route('/winner')
     def winner():
+        ui.winner = ui.wname if board.turn == "white" else ui.bname
         return render_template('winner.html',ui=ui)
 
             
